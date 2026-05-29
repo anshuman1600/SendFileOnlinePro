04c960412e1acf5e60115ed34187791269098f3c
Anshuman Singh <thakuranshumansingh945@gmail.com>
2026-05-29 20:55:40 +0530

    Add unhandled rejection error handlers to server

--- server/src/index.js
+++ server/src/index.js
@@ -111,5 +111,15 @@ connectMongo().finally(() => {
   });
 });
 
+// Graceful shutdown and error handling
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
 
 
